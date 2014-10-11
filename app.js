565f54b7bfee05d18d260c2a9594428b6e07da41
Lingfu Zhang <lf.zhang86@gmail.com>
2014-10-11 15:52:57 -0700

    The Posts Page

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ angular.module('flapperNews', ['ui.router'])
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url:'/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
       });
 
     $urlRouterProvider.otherwise('home');
@@ -34,6 +39,7 @@ function ($scope, posts) {
       link: $scope.link,
       upvotes: 0
     });
+    
   	$scope.title = '';
     $scope.link = '';
   };
@@ -42,4 +48,10 @@ function ($scope, posts) {
     post.upvotes += 1;
   };
 
-}]);
+}])
+.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts', function ($scope, $stateParams, posts) {
+
+  }])
